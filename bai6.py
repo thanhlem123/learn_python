39bcc42bba4f91a8a04d98089a895de9a7fcc415
thanhlem123 <ngoxuanthanh26@gmail.com>
2022-04-04 23:14:13 +0700

    feature: bai6

---
+++ bai6.py
@@ -0,0 +1,13 @@
+
+#phân sô
+from fractions import*
+fract = Fraction(5,10)
+print(fract)
+print(type(fract))
+
+#số phức
+#cấu trức < phần thực> + < phần ảo>j
+c = complex(2,5)
+print(c)
+print(c.real) #lấy phần thưjc
+print(c.imag) #lấy phần ảo
